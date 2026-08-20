2e452b02487ca850d25e4b4cbb24bb87bf2d3b81
Abu Sayed Khudry <khudryabusayed@gmail.com>
2026-08-20 21:50:35 +0600

    modify a problem

--- Codeforces Assuit Sheet/E_Maximum_Distinct_Numbers.cpp
+++ Codeforces Assuit Sheet/E_Maximum_Distinct_Numbers.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main() {
+int main()
+{
     long long N;
-    if (cin >> N) {
+    if (cin >> N)
+    {
         // Direct quadratic equation solution
         long long k = (-1 + sqrt(1 + 8.0 * N)) / 2;
 
         // Adjust for floating-point precision edge cases
-        while ((k + 1) * (k + 2) / 2 <= N) {
+        while ((k + 1) * (k + 2) / 2 <= N)
+        {
             k++;
         }
-        while (k * (k + 1) / 2 > N) {
+        while (k * (k + 1) / 2 > N)
+        {
             k--;
         }
         cout << k << "\n";
