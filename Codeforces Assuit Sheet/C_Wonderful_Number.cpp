5295f87afdb8cbf665b35ad8eabcba4c02ae2a9f
Abu Sayed Khudry <khudryabusayed@gmail.com>
2026-08-22 23:48:48 +0600

    modify a problem

--- Codeforces Assuit Sheet/C_Wonderful_Number.cpp
+++ Codeforces Assuit Sheet/C_Wonderful_Number.cpp
@@ -1,24 +1,88 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+// void fun(int N, long long &bi1, long long &bi2)
+// {
+//     if (N == 0)
+//     {
+//         return;
+//     }
+//     int bi = N % 2;
+//     bi1 = bi1 * 10 + bi;
+//     fun(N / 2, bi1, bi2);
+//     bi2 = bi2 * 10 + bi;
+// }
+// int main()
+// {
+//     int N;
+//     cin >> N;
+//     long long bi1 = 0;
+//     long long bi2 = 0;
+//     fun(N, bi1, bi2);
+//     if (bi1 == bi2)
+//     {
+//         cout << "YES\n";
+//     }
+//     else
+//     {
+//         cout << "NO\n";
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+#include <string>
+
 using namespace std;
-void fun(int N, long long &bi1, long long &bi2)
+
+// Function 1: Checks if the number is odd
+bool isOdd(int N)
+{
+    return N % 2 != 0;
+}
+
+// Helper function to build the binary string recursively
+void getBinary(int N, string &binary)
 {
     if (N == 0)
     {
         return;
     }
-    int bi = N % 2;
-    bi1 = bi1 * 10 + bi;
-    fun(N / 2, bi1, bi2);
-    bi2 = bi2 * 10 + bi;
+    // Append the bit
+    binary += (N % 2 == 0 ? '0' : '1');
+    getBinary(N / 2, binary);
 }
+
+// Function 2: Checks if the binary representation is a palindrome
+bool isPalindrome(int N)
+{
+    string binary = "";
+    getBinary(N, binary);
+
+    int left = 0;
+    int right = binary.length() - 1;
+
+    while (left < right)
+    {
+        if (binary[left] != binary[right])
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int N;
     cin >> N;
-    long long bi1 = 0;
-    long long bi2 = 0;
-    fun(N, bi1, bi2);
-    if (bi1 == bi2)
+
+    if (isOdd(N) && isPalindrome(N))
     {
         cout << "YES\n";
     }
@@ -26,5 +90,6 @@ int main()
     {
         cout << "NO\n";
     }
+
     return 0;
 }
